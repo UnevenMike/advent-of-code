987781165cc19603f8766620d91ce26579a0df26
Michael Butterfield <michaelbutterfield1996@gmail.com>
2022-12-04 13:58:59 -0600

    Add day four solutions. Not difficult at all

---
+++ aoc-2022/day-04/b.ts
@@ -0,0 +1,37 @@
+ 
+// deno-lint-ignore-file prefer-const
+// Read file
+import { readLines } from 'https://deno.land/std@0.167.0/io/buffer.ts';
+const f = await Deno.open('./input.txt');
+const lines: string[] = [];
+for await (const l of readLines(f)) {
+    lines.push(l);
+}
+
+// Begin Problem solving
+
+const data = lines.map(line => {
+    const pairs = line.split(",");
+    const ranges = pairs.map(range => {
+        const rangeSplit = range.split("-");
+        return { 
+            min: Number.parseInt(rangeSplit[0]),
+            max: Number.parseInt(rangeSplit[1])
+        };
+    });
+    return ranges;
+});
+
+let count = 0;
+data.forEach(pair => {
+    if (pair[0].min <= pair[1].max && pair[0].max >= pair[1].min ) {
+        count += 1;
+    } else if (pair[1].min <= pair[0].max && pair[1].max >= pair[0].min) {
+        count += 1;
+    }
+});
+
+// Set solution and print
+
+const solution = count;
+console.log('Solution:', solution);
