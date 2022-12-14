af0a8d13a39a8f1b5c258b00efe0ad464ff7e033
Michael Butterfield <michaelbutterfield1996@gmail.com>
2022-12-13 22:31:29 -0600

    Some recursive comparisons

--- aoc-2022/day-13/a.ts
+++ aoc-2022/day-13/a.ts
@@ -9,11 +9,49 @@ for await (const l of readLines(f)) {
 
 // Begin Problem solving
 
-console.log(lines)
+function compareLists(first: any, second: any): boolean | null{
+    let isFirstArray = Array.isArray(first);
+    let isSecondArray = Array.isArray(second);
+    let isCorrect = null;
 
+    if (!isFirstArray && !isSecondArray) {
+        isCorrect = first < second ? true : first > second ? false : null;
+    }
+    else if (isFirstArray && !isSecondArray) {
+        isCorrect = compareLists(first, [second]);
+    }
+    else if (!isFirstArray && isSecondArray) {
+        isCorrect = compareLists([first], second);
+    }
+    else {
+        let minLength = Math.min(first.length, second.length);
+        for(let i = 0; i < minLength; i++) {
+            isCorrect = compareLists(first[i], second[i]);
+            if (isCorrect != null) {
+                return isCorrect;
+            }
+        } 
+        if(isCorrect == null) {
+            isCorrect = first.length < second.length ? true : first.length > second.length ? false : null;
+        }
+    }
+    return isCorrect;
+}
 
+let pairs = [];
+for (let i = 0; i < lines.length; i += 3) {
+    pairs.push({ first: JSON.parse(lines[i]), second: JSON.parse(lines[i+1]) });
+}
+
+let indexes: number[] = []
+pairs.forEach((pair, index) => {
+    let res = compareLists(pair.first, pair.second);
+    if (res) {
+        indexes.push(index + 1);
+    }
+});
 
 // Set solution and print
-
-const solution = 0;
+const sum = indexes.reduce((prev, curr) => prev + curr, 0);
+const solution = sum;
 console.log('Solution:', solution);
--- aoc-2022/day-13/b.ts
+++ aoc-2022/day-13/b.ts
@@ -8,12 +8,53 @@ for await (const l of readLines(f)) {
 }
 
 // Begin Problem solving
+function compareLists(first: any, second: any): boolean | null{
+    let isFirstArray = Array.isArray(first);
+    let isSecondArray = Array.isArray(second);
+    let isCorrect = null;
 
-console.log(lines)
+    if (!isFirstArray && !isSecondArray) {
+        isCorrect = first < second ? true : first > second ? false : null;
+    }
+    else if (isFirstArray && !isSecondArray) {
+        isCorrect = compareLists(first, [second]);
+    }
+    else if (!isFirstArray && isSecondArray) {
+        isCorrect = compareLists([first], second);
+    }
+    else {
+        let minLength = Math.min(first.length, second.length);
+        for(let i = 0; i < minLength; i++) {
+            isCorrect = compareLists(first[i], second[i]);
+            if (isCorrect != null) {
+                return isCorrect;
+            }
+        } 
+        if(isCorrect == null) {
+            isCorrect = first.length < second.length ? true : first.length > second.length ? false : null;
+        }
+    }
+    return isCorrect;
+}
 
+let data = [];
+for(let i = 0; i<lines.length; i++) {
+    if (lines[i]) {
+        data.push(JSON.parse(lines[i]));
+    }
+}
 
+const data2 = [[2]];
+const data6 = [[6]];
+data.push(data2);
+data.push(data6);
+
+data.sort((a, b) => compareLists(a,b) ? -1 : 1);
+
+let idx2 = data.indexOf(data2) + 1;
+let idx6 = data.indexOf(data6) + 1;
 
 // Set solution and print
 
-const solution = 0;
+const solution = idx2 * idx6;
 console.log('Solution:', solution);
